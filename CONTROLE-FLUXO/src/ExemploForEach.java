5ad957adeefa4080aceea454d57a1e87be5a87fb
Thiago Moisés Braga Montalvão <111362865+Thiago-Moises@users.noreply.github.com>
2024-03-22 12:45:11 -0300

    Add files via upload

---
+++ CONTROLE-FLUXO/src/ExemploForEach.java
@@ -0,0 +1,17 @@
+public class ExemploForEach {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String alunos[] = { "FELIPE", "THIAGO", "JULIA", "THAMIRIS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+
+            System.out.println("Nome do aluno é : " + x + " é " + alunos[x]);
+        }
+
+    }
+
+}
---
+++ CONTROLE-FLUXO/src/ExmploForArrays.java
@@ -0,0 +1,13 @@
+public class ExmploForArrays {
+
+    public static void main(String[] args) {
+
+        String alunos[] = { "FELIPE", "THIAGO", "JULIA", "THAMIRIS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+
+            System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
+    }
+
+}
